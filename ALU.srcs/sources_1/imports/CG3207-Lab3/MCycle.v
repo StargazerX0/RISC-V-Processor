39cec98d663c98fa8ad9b877c53612cc7b390458
LAPTOP-D31L41D6\elyovs <elyovsun@gmail.com>
2024-10-12 17:00:57 +0800

    karatsuba algo improvement on mul

--- ALU.srcs/sources_1/imports/CG3207-Lab3/MCycle.v
+++ ALU.srcs/sources_1/imports/CG3207-Lab3/MCycle.v
@@ -60,7 +60,9 @@ module MCycle
     reg [7:0] count = 0 ; // assuming no computation takes more than 256 cycles.
     reg [2*width-1:0] temp_sum = 0 ;
     reg [2*width-1:0] shifted_op1 = 0 ;
-    reg [2*width-1:0] shifted_op2 = 0 ;     
+    reg [2*width-1:0] shifted_op2 = 0 ;
+         
+    parameter half_width = 16; // also will cause the above warning
    
     always@( state, done, Start, RESET ) begin : IDLE_PROCESS  
 		// Note : This block uses non-blocking assignments to get around an unpredictable Verilog simulation behaviour.
@@ -94,32 +96,89 @@ module MCycle
     reg op1_sign = 0;
     reg op2_sign = 0;
 
+    wire [half_width-1:0] op1_msb;
+    assign op1_msb = Operand1[width-1:half_width];
+    wire [half_width-1:0] op1_lsb;
+    assign op1_lsb = Operand1[half_width-1:0];
+    
+    wire [half_width-1:0] op2_msb;
+    assign op2_msb = Operand2[width-1:half_width];
+    wire [half_width-1:0] op2_lsb;
+    assign op2_lsb = Operand2[half_width-1:0];
+    
+    reg [width-1:0] shifted_op1_msb = 0;
+    reg [width-1:0] shifted_op1_lsb = 0;
+    reg [width-1:0] shifted_op2_msb = 0;
+    reg [width-1:0] shifted_op2_lsb = 0;
+    reg [width-1:0] temp_sum_msb = 0;
+    reg [width-1:0] temp_sum_lsb = 0;
+    
+    wire [half_width-1:0] sum_op1;
+    assign sum_op1 = op1_msb + op1_lsb;
+    wire [half_width-1:0] sum_op2;
+    assign sum_op2 = op2_msb + op2_lsb; 
+    
+    reg [width-1:0] temp_sum_sum_op = 0;
+    reg [width-1:0] shifted_sum_op1 = 0;
+    reg [width-1:0] shifted_sum_op2 = 0;
+    
+    reg [width-1:0] Z1 = 0;
+    reg [2*width-1:0] result = 0;
+
     
     always@( posedge CLK ) begin : COMPUTING_PROCESS // process which does the actual computation
         // n_state == COMPUTING and state == IDLE implies we are just transitioning into COMPUTING
         if( RESET | (n_state == COMPUTING & state == IDLE) ) begin // 2nd condition is true during the very 1st clock cycle of the multiplication
             count = 0 ;
             temp_sum = 0 ;
+            temp_sum_msb = 0;
+            temp_sum_lsb = 0;
+            temp_sum_sum_op = 0;
 
             if (~MCycleOp[0]) begin // Signed operation, simple trick to convert
                 // Store signs and convert to positive
                 op1_sign = Operand1[width-1];
                 op2_sign = Operand2[width-1];
-                if (op1_sign)
-                    shifted_op1 = {{width{1'b0}}, (~Operand1 + 1'b1)};
-                else
-                    shifted_op1 = {{width{1'b0}}, Operand1};
                 
-                if (op2_sign)
+                if (op1_sign) begin
+                    shifted_op1 = {{width{1'b0}}, (~Operand1 + 1'b1)};
+                    shifted_op1_msb = {{half_width{1'b0}}, (~op1_msb)};
+                    shifted_op1_lsb = {{half_width{1'b0}}, (~op1_lsb) + 1'b1};
+                    shifted_sum_op1 = {{half_width{1'b0}}, (~sum_op1 + 1'b1)};
+                end
+                else begin
+                    shifted_op1 = {{width{1'b0}}, Operand1};
+                    shifted_op1_msb = {{half_width{1'b0}}, (op1_msb)};
+                    shifted_op1_lsb = {{half_width{1'b0}}, (op1_lsb)};
+                    shifted_sum_op1 = {{half_width{1'b0}}, (sum_op1)};
+                end
+                
+                if (op2_sign) begin
                     shifted_op2 = {{width{1'b0}}, (~Operand2 + 1'b1)};
-                else
+                    shifted_op2_msb = {{half_width{1'b0}}, (op2_msb + 1'b1)};
+                    shifted_op2_lsb = {{half_width{1'b0}}, (op2_lsb) + 1'b1};
+                    shifted_sum_op2 = {{half_width{1'b0}}, (~sum_op2 + 1'b1)};
+                end
+                else begin
                     shifted_op2 = {{width{1'b0}}, Operand2};
+                    shifted_op2_msb = {{half_width{1'b0}}, (op2_msb)};
+                    shifted_op2_lsb = {{half_width{1'b0}}, (op2_lsb)};
+                    shifted_sum_op2 = {{half_width{1'b0}}, (sum_op2)};
+                end
 
             end else begin // Unsigned operation
                 op1_sign = 1'b0;
                 op2_sign = 1'b0;
                 shifted_op1 = {{width{1'b0}}, Operand1};
                 shifted_op2 = {{width{1'b0}}, Operand2};
+                
+                shifted_op1_msb = {{half_width{1'b0}}, (Operand1[width-1:half_width])};
+                shifted_op1_lsb = {{half_width{1'b0}}, (Operand1[half_width-1:0])};
+                shifted_sum_op1 = {{half_width{1'b0}}, (sum_op1)};
+                
+                shifted_op2_msb = {{half_width{1'b0}}, (Operand2[width-1:half_width])};
+                shifted_op2_lsb = {{half_width{1'b0}}, (Operand2[half_width-1:0])};
+                shifted_sum_op2 = {{half_width{1'b0}}, (sum_op2)};
             end
 
         end ;
@@ -129,17 +188,43 @@ module MCycle
         if( ~MCycleOp[1] ) begin // Multiply
             // if( ~MCycleOp[0] ), takes 2*'width' cycles to execute, returns signed(Operand1)*signed(Operand2)
             // if( MCycleOp[0] ), takes 'width' cycles to execute, returns unsigned(Operand1)*unsigned(Operand2)        
-            if( shifted_op2[0] ) // add only if b0 = 1
-                temp_sum = temp_sum + shifted_op1 ; // partial product for multiplication
+//            if( shifted_op2[0] ) // add only if b0 = 1
+//                temp_sum = temp_sum + shifted_op1 ; // partial product for multiplication
+            
+//            shifted_op2 = {1'b0, shifted_op2[2*width-1 : 1]} ;
+//            shifted_op1 = {shifted_op1[2*width-2 : 0], 1'b0} ;
+            
+            if (shifted_op2_msb[0])
+                temp_sum_msb = temp_sum_msb + shifted_op1_msb;
                 
-            shifted_op2 = {1'b0, shifted_op2[2*width-1 : 1]} ;
-            shifted_op1 = {shifted_op1[2*width-2 : 0], 1'b0} ;    
+            if (shifted_op2_lsb[0])
+                temp_sum_lsb = temp_sum_lsb + shifted_op1_lsb;
+            
+            if (shifted_sum_op2[0])
+                temp_sum_sum_op = temp_sum_sum_op + shifted_sum_op1;
+            
+            shifted_op2_msb = {1'b0, shifted_op2_msb[width-1 : 1]};
+            shifted_op2_lsb = {1'b0, shifted_op2_lsb[width-1 : 1]};
+            shifted_sum_op2 = {1'b0, shifted_sum_op2[width-1 : 1]};
+            shifted_op1_msb = {shifted_op1_msb[width-2 : 0], 1'b0};
+            shifted_op1_lsb = {shifted_op1_lsb[width-2 : 0], 1'b0};
+            shifted_sum_op1 = {shifted_sum_op1[width-2 : 0], 1'b0};     
                 
-        if (count == 2*width-1) begin
+//        if (count == 2*width-1) begin
+//            done <= 1'b1;
+//            // Negate result if signs are different (only for signed multiplication)
+//            if (~MCycleOp[0] && (op1_sign ^ op2_sign))
+//                temp_sum = ~temp_sum + 1'b1; // 2's complement negation
+//        end
+        
+        if (count == width-1) begin
             done <= 1'b1;
-            // Negate result if signs are different (only for signed multiplication)
+            
+            Z1 = temp_sum_sum_op - temp_sum_msb - temp_sum_lsb;
+            result = (temp_sum_msb << width) + (temp_sum_sum_op << half_width) + temp_sum_lsb;
+            
             if (~MCycleOp[0] && (op1_sign ^ op2_sign))
-                temp_sum = ~temp_sum + 1'b1; // 2's complement negation
+                temp_sum = ~result + 1'b1; // 2's complement negation
         end
                
             count = count + 1;    
