6325683e6755331aa599b52566c3fa3277b8cbbc
Koh Ngiap Hin <64647635+kohnh@users.noreply.github.com>
2024-10-13 01:54:22 +0800

    Update Wrapper.v

--- ALU.srcs/sources_1/imports/Processor/Wrapper.v
+++ ALU.srcs/sources_1/imports/Processor/Wrapper.v
@@ -98,18 +98,18 @@ reg [31:0] i;
 // Instruction Memory
 //----------------------------------------------------------------
 initial begin
-	INSTR_MEM[0] = 32'h00500413;
-	INSTR_MEM[1] = 32'hffd00493;
-	INSTR_MEM[2] = 32'h02940933;
-	INSTR_MEM[3] = 32'h80000437;
-	INSTR_MEM[4] = 32'h00040413;
-	INSTR_MEM[5] = 32'h00200493;
-	INSTR_MEM[6] = 32'h029439b3;
-	INSTR_MEM[7] = 32'h01400413;
-	INSTR_MEM[8] = 32'h00300493;
-	INSTR_MEM[9] = 32'h02945a33;
+	INSTR_MEM[0] = 32'h00002917;
+	INSTR_MEM[1] = 32'h40490913;
+	INSTR_MEM[2] = 32'h00092983;
+	INSTR_MEM[3] = 32'h00200a13;
+	INSTR_MEM[4] = 32'h00010ab7;
+	INSTR_MEM[5] = 32'hfffa8a93;
+	INSTR_MEM[6] = 32'h03498b33;
+	INSTR_MEM[7] = 32'h035a8bb3;
+	INSTR_MEM[8] = 32'h0349dc33;
+	INSTR_MEM[9] = 32'h035adcb3;
 	INSTR_MEM[10] = 32'h000000ef;
-	for (i = 35; i < 128; i = i + 1) begin
+	for (i = 11; i < 128; i = i + 1) begin
 		INSTR_MEM[i] = 32'h0;
 	end
 end
@@ -126,7 +126,7 @@ initial begin
 	DATA_CONST_MEM[5] = 32'h2e373032;
 	DATA_CONST_MEM[6] = 32'h000a0d2e;
 	DATA_CONST_MEM[7] = 32'h00000005;
-	for (i = 10; i < 128; i = i + 1) begin
+	for (i = 8; i < 128; i = i + 1) begin
 		DATA_CONST_MEM[i] = 32'h0;
 	end
 end
