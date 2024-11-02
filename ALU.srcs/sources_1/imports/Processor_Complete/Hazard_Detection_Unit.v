2ab3884d9a9fa0685376dafd2eaaadd42850c728
StargazerX0 <lkjsgu@outlook.com>
2024-11-02 17:13:02 +0800

    hazard

--- ALU.srcs/sources_1/imports/Processor_Complete/Hazard_Detection_Unit.v
+++ ALU.srcs/sources_1/imports/Processor_Complete/Hazard_Detection_Unit.v
@@ -18,18 +18,18 @@ module Hazard_Detection_Unit(
         Stall = 1'b0;
         Flush = 1'b0;
 
-        // // Load-Use Hazard: If the EX stage is loading a register that the ID stage needs
-        // if (EX_MemRead && ((EX_RD == ID_RS1) || (EX_RD == ID_RS2))) begin
-        //     Stall = 1'b1; // Stall the pipeline
-        // end
-
-        // // Store-Load Hazard: If the EX stage is storing and the ID stage is loading
-        // if (EX_MemWrite && ID_MemRead) begin
-        //     Stall = 1'b1; // Stall the pipeline
-        // end
+        // Load-Use Hazard: If the EX stage is loading a register that the ID stage needs
         if (EX_MemRead && ((EX_RD == ID_RS1) || (EX_RD == ID_RS2))) begin
             Stall = 1'b1; // Stall the pipeline
         end
+
+        // Store-Load Hazard: If the EX stage is storing and the ID stage is loading
+        if (EX_MemWrite && ID_MemRead) begin
+            Stall = 1'b1; // Stall the pipeline
+        end
+        // if (EX_MemRead && ((EX_RD == ID_RS1) || (EX_RD == ID_RS2))) begin
+        //     Stall = 1'b1; // Stall the pipeline
+        // end
 
         // Control Hazard: If a branch is taken, flush the IF/ID pipeline register
         if (PCSrc_EX != 2'b00) begin
